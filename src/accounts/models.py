71f1068286d81a80ec78948025f7e760743d4806
arun-arunisto <arun.arunisto2@gmail.com>
2023-07-07 14:47:40 +0530

    admin file

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -16,11 +16,14 @@ class User(UserMixin, db.Model):
     created_on = db.Column(db.DateTime, nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
+
     def __init__(self, email, password, is_admin=False):
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
         self.created_on = datetime.now()
         self.is_admin = is_admin
+
+
 
     def __repr__(self):
         return f"<email {self.email}>"
